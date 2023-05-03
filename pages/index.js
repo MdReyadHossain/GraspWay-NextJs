82bf019edc93bd72cdc7aafcb60970b9715fcaa1
Reyad <69958555+MdReyadHossain@users.noreply.github.com>
2023-05-04 03:48:34 +0600

    dependency fix

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,8 @@
 import LayoutHome from "@/components/_layoutHome"
+import React from 'react';
+import { Player } from 'video-react';
+import { CldVideoPlayer } from 'next-cloudinary';
+
 
 export default function Home() {
     return (
